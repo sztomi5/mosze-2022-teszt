87df4089d92c8aa75227aebb2ab35447c3eee497
Szabó Tamás <sztomi5@gmail.com>
2022-09-13 15:33:13 +0200

    Hibas sorok jelzese

--- main.cpp
+++ main.cpp
@@ -5,20 +5,20 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    std::cout << '1-100 ertekek duplazasa' //Nincsen sortores
+    for (int i = 0;) //Befejezetlen itaracio
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //Ez esetben 0-99-ig duplazza az ertekeket
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //Nincsen feltetel az iteracio kileptetesere
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //Nem iratunk ki ertekeket
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; //Nincsen kezdo ertek
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //Pontosvesszo hianya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
